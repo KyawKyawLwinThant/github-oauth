5c5d0474fe0bb1552e64cbed47837adcd59d2fb4
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2021-08-05 10:40:51 +0630

    second commit

--- src/main/java/com/example/demo4oauth/Demo4OauthApplication.java
+++ src/main/java/com/example/demo4oauth/Demo4OauthApplication.java
@@ -8,6 +8,7 @@ public class Demo4OauthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo4OauthApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
